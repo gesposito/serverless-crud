67842e96f074a1ceee3dd8ab73704c9a0d938746
Gianluca Esposito <g@esposi.to>
2016-12-31 18:11:28 +0100

    Add DynamoDB Resource, Add IAM Role

--- handler.js
+++ handler.js
@@ -1,15 +1,29 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+var AWS       = require('aws-sdk');
+var dynamodb  = new AWS.DynamoDB();
 
-  callback(null, response);
+var params = {
+  "TableName": 'greetings'
+};
+
+module.exports.hello = (event, context, callback) => {
+
+  dynamodb.scan(params, function(err, data) {
+    if (err) {
+      console.error(err, err.stack);
+      callback(err);
+    } else {
+      const response = {
+        "statusCode": 200,
+        "body": JSON.stringify({
+          "messages": data,
+        }),
+      };
+
+      callback(null, response);
+    }
+  });
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
